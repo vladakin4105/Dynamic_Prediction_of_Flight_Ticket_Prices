9d742beaeb1b184b8df9f67fb138ecec7a7be976
madalinapal <mdlpalade@gmail.com>
2024-12-06 00:33:44 +0200

    form almost done

--- frontend/script.js
+++ frontend/script.js
@@ -7,3 +7,7 @@ function closeSidebar() {
   const sideBar = document.querySelector(".sidebar");
   sideBar.style.display = "none";
 }
+
+//the input date must be at least today's date
+const today = new Date().toISOString().split("T")[0];
+document.getElementById("departure-date").setAttribute("min", today);
