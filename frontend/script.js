977ab11c1b7c72c1b5639793fc3902f12e4bc72e
madalinapal <mdlpalade@gmail.com>
2024-12-05 23:24:50 +0200

    header done

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+  const sideBar = document.querySelector(".sidebar");
+  sideBar.style.display = "flex";
+}
+
+function closeSidebar() {
+  const sideBar = document.querySelector(".sidebar");
+  sideBar.style.display = "none";
+}
